a0796169e3677377e0e5be17e9cd95ace7719b23
MuratovAS <lexamzx@gmail.com>
2024-01-28 15:18:05 +0500

    add support dwlb

--- noticat.c
+++ noticat.c
@@ -23,6 +23,53 @@ void print_json(char const *appname, uint32_t id, char const *icon, char const *
 }
 #endif
 
+#ifdef DWLB_STRLEN
+
+void print_dwlb(char const *appname, uint32_t id, char const *icon, char const *summary,
+                        char const *body, int32_t timeout) {  
+  char *_summary = (char*)summary;
+  char *_body = (char*)body;
+
+  uint32_t len = DWLB_STRLEN;
+  
+  printf("[%s] ", appname);
+
+  while (*_summary != '\0') {
+    if (len != 0)
+      len--;
+    else 
+      break;
+    if (*_summary != '\n' && *_summary != '\r')
+      putchar(*_summary);
+    else 
+      putchar(' ');      
+    _summary++;
+  }
+
+  if (*_body != '\0' && len != 0) {
+    printf(":");
+    len--;
+  }
+   
+  while (*_body != '\0') {
+    if (len != 0)
+      len--;
+    else 
+      break;
+    if (*_body != '\n' && *_body != '\r')
+      putchar(*_body);
+    else 
+      putchar(' ');
+    _body++;
+  }
+
+  if (len == 0)
+    printf("+");  
+  putchar('\n');
+  fflush(stdout);
+}
+#endif
+
 void print_notification(char const *appname, uint32_t id, char const *icon, char const *summary,
                         char const *body, int32_t timeout) {
   printf("\"%s\" %u \"%s\" \"%s\" \"%s\" %d\n", appname, id, icon, summary, body, timeout);
@@ -32,16 +79,23 @@ void print_notification(char const *appname, uint32_t id, char const *icon, char
 int main(int argc, char *argv[]) {
   cnoti_callback_type *callback = print_notification;
 
-  if (argc > 1) {
-    if (argc == 2 && strcmp(argv[1], "-json") == 0) {
+  if (argc == 2 ) {
+    if (strcmp(argv[1], "-json") == 0) {
 #ifdef USE_CJSON
       callback = print_json;
 #else
       printf("JSON option not available in this build, please build with USE_CJSON=YES to use it.\n");
       return 1;
 #endif // CJSON
+    } else if (strcmp(argv[1], "-dwlb") == 0) {
+#ifdef DWLB_STRLEN
+      callback = print_dwlb;
+#else
+      printf("DWLB option not available in this build, please build with DWLB_STRLEN=64 to use it.\n");
+      return 1;
+#endif // DWLB
     } else {
-      printf("Usage: %s [-json]\n", argv[0]);
+      printf("Usage: %s [-json] [-dwlb]\n", argv[0]);
       return 0;
     }
   }
